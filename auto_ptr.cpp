019753dcc74de523eb4f75a4767d3c0a63a662ec
williamaiden <williamaiden@qq.com>
2017-06-05 20:44:16 +0800

    Update auto_ptr.cpp

--- auto_ptr.cpp
+++ auto_ptr.cpp
@@ -20,13 +20,35 @@ void my_shared_ptr(){
 	sptr->show();
 }
 
+void auto_run_fun(FILE* f){
+	printf("auto running.\n");
+}
+
+void my_shared_ptr2(){
+	FILE* f = nullptr;
+	std::shared_ptr<FILE> file_ptr(f, auto_run_fun);
+}
+
+void my_shared_ptr3(){
+	demo*
+}
+
 void my_unique_ptr(){
 	auto uptr = std::make_unique<int[]>(10);
 	uptr[5] = 17;
 	printf("%d\n", uptr[5]);
 }
 
+void my_unique_ptr2(){
+	auto uptr = std::make_unique<demo>();
+	uptr->show();
+	std::unique_ptr<demo> uptr2(new demo());///写法等效
+	uptr2->show();
+}
+
 int main(void){
-	my_shared_ptr();
+	///my_shared_ptr();
+	///my_unique_ptr2();
+	my_shared_ptr2();
 	return 0;
 }
